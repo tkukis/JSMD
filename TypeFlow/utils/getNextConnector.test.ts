76799d5fca35b782059baffdce3c8ece8c4cea3e
Tomas Kukis <tkukis@kukis.net>
2021-07-02 08:27:29 +0300

    added getNextConnector

--- TypeFlow/utils/getNextConnector.test.ts
+++ TypeFlow/utils/getNextConnector.test.ts
@@ -1,11 +1,12 @@
 import getNextConnector from "./getNextConnector"
 
-describe.only("getNextConnector", () => {
-    test.only('getNextConnector', async () => {
+describe("getNextConnector", () => {
+    test('getNextConnector', async () => {
         expect(getNextConnector({}, [{ start: "a", end: "b" }], "a")?.end).toBe("b")
         expect(getNextConnector({}, [{ start: "c", end: "b" }], "a")?.end).toBe(undefined)
 
         expect(getNextConnector({}, [{ start: "a", end: "b", condition: "{{it.state.a.b===100}}" }], "a")?.end).toBe(undefined)
-        expect(getNextConnector({ a: { b: 100 } }, [{ start: "a", end: "b", condition: "{{it.state.a.b===100}}" }], "a")?.end).toBe("b")
+        expect(getNextConnector({ state: { a: { b: 100 } } }, [{ start: "a", end: "b", condition: "{{it.state.a.b===100}}" }], "a")?.end).toBe("b")
+        expect(getNextConnector({ state: { a: { b: 100 } } }, [{ start: "a", end: "b", condition: "{{it.state.a.b===101}}" }], "a")?.end).toBe(undefined)
     })
 })
